9b74002cde344d8f8f91f5565a69e5d77f5eeee9
박준재 <wnswo1109@naver.com>
2025-01-10 23:37:03 +0900

    시큐리티 설정

---
+++ src/main/java/project/book/WelcomController.java
@@ -0,0 +1,13 @@
+package project.book;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomController {
+
+    @GetMapping("/welcome")
+    public String welcomeMessage() {
+        return "Welcome to the Book Application!";
+    }
+}
